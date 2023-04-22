2d14b049cfa2b75100bc997ebd5a6a12960c0757
Bryan Gruneberg <hello@bryangruneberg.com>
2023-04-22 15:10:59 -0600

    Basic build of LND & TOR

--- lnd/entrypoints/99-entrypoint-lnd.sh
+++ lnd/entrypoints/99-entrypoint-lnd.sh
@@ -4,5 +4,11 @@ set -e
 echo Checking for any LND specific entrypoint requirements
 
 if [ ! -d "/app/storage/lnd" ]; then
+	echo Created the LND directory
 	mkdir -p /app/storage/lnd
+fi
+
+if [ ! -f "/app/storage/lnd/wallet_password" ]; then
+	echo Created a LND wallet password
+	openssl rand -hex 21 > /app/storage/lnd/wallet_password
 fi
