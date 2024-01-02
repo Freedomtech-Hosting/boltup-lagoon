8e5d540df35c0adf408df73b7ac579372ce5c95f
Michael Schmid <michael@amazee.com>
2024-01-02 17:30:14 -0500

    switch to use bitcoind

--- lnd/start-lnd.sh
+++ lnd/start-lnd.sh
@@ -9,50 +9,15 @@ echo "LND_ALIAS=${LND_ALIAS}"
 ep /app/lnd.conf
 
 
-# Adding neutrino peers based on selected network
-if [ "$BITCOIN_NETWORK" == "mainnet" ]; then
-    cat <<EOT >> /app/lnd.conf
-
-[neutrino]
-# Mainnet addpeers
-neutrino.addpeer=btcd-mainnet.lightning.computer
-neutrino.addpeer=mainnet1-btcd.zaphq.io
-neutrino.addpeer=mainnet2-btcd.zaphq.io
-neutrino.addpeer=mainnet3-btcd.zaphq.io
-neutrino.addpeer=mainnet4-btcd.zaphq.io
-neutrino.feeurl=https://nodes.lightning.computer/fees/v1/btc-fee-estimates.json
-EOT
-
-elif [ "$BITCOIN_NETWORK" == "testnet" ]; then
-    cat <<EOT >> /app/lnd.conf
-
-[neutrino]
-# Testnet addpeers
-neutrino.addpeer=btcd-testnet.lightning.computer
-neutrino.addpeer=lnd.bitrefill.com:18333
-neutrino.addpeer=faucet.lightning.community
-neutrino.addpeer=testnet1-btcd.zaphq.io
-neutrino.addpeer=testnet2-btcd.zaphq.io
-neutrino.addpeer=testnet3-btcd.zaphq.io
-neutrino.addpeer=testnet4-btcd.zaphq.io
-neutrino.feeurl=https://nodes.lightning.computer/fees/v1/btctestnet-fee-estimates.json
-EOT
-
-else
-
-    echo "Unknown BITCOIN_NETWORK variable set to ${BITCOIN_NETWORK}, failing"
-    exit 1
-fi
-
-# Create rpc.cert for btcd node. If the LND_BTCD_RPCCERT env variable is empty,
-# ensure we have an empty cert, as then lnd will use public cert authorities
-if [ -z "$LND_BTCD_RPCCERT" ]
-then
-      rm -f /app/storage/rpc.cert
-      touch /app/storage/rpc.cert
-else
-      echo -e "$LND_BTCD_RPCCERT" > /app/storage/rpc.cert
-fi
+# # Create rpc.cert for btcd node. If the LND_BTCD_RPCCERT env variable is empty,
+# # ensure we have an empty cert, as then lnd will use public cert authorities
+# if [ -z "$LND_BTCD_RPCCERT" ]
+# then
+#       rm -f /app/storage/rpc.cert
+#       touch /app/storage/rpc.cert
+# else
+#       echo -e "$LND_BTCD_RPCCERT" > /app/storage/rpc.cert
+# fi
 
 
 if [[ ! -d /app/storage/lnd ]]; then
